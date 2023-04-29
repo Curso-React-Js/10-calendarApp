96c4b9a9c2925c836ebadf03372a15a3fb0c7891
Luis Angel <angelfgdeveloper@gmail.com>
2023-04-28 20:30:49 -0600

    Pruebas iniciales en useAuthStore

---
+++ test/hooks/useAuthStore.test.js
@@ -0,0 +1,42 @@
+import { Provider } from 'react-redux';
+import { renderHook } from '@testing-library/react';
+import { configureStore } from '@reduxjs/toolkit';
+
+import { authSlice } from '../../src/store';
+import { initialState } from '../fixtures/authStates';
+import { useAuthStore } from '../../src/hooks/useAuthStore';
+
+const getMockStore = ( initialState ) => {
+  return configureStore({
+    reducer: {
+      auth: authSlice.reducer,
+    },
+    preloadedState: {
+      auth: { ...initialState }
+    }
+  });
+}
+
+describe('Pruebas en useAuthStore', () => {
+  
+  test('debe de regresar los valores por defecto', () => {
+
+    const mockStore = getMockStore({ ...initialState });
+
+    const { result } = renderHook(() => useAuthStore() , {
+      wrapper: ({ children }) => <Provider store={ mockStore }>{ children }</Provider>
+    });
+
+    expect( result.current ).toEqual({
+      errorMessage: undefined,
+      status: 'checking',
+      user: {},
+      checkAuthToken: expect.any(Function),
+      startLogin: expect.any(Function),
+      startLogout: expect.any(Function),
+      startRegister: expect.any(Function),
+    });
+    
+  });
+  
+});
